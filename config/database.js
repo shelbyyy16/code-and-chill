b881a213b25095c4c05840052d0fc552f99902f5
doniellekinchen <donicodes.things@gmail.com>
2023-11-20 11:12:12 -0500

    schemas have been made

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'); 
+// const connectionString = process.env.DATABASE_URL
+mongoose.connect(process.env.DATABASE_URL)
+console.log(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+db.on('connected', function () {
+console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`) 
+})
+
+db.on('error', function(err){
+    console.log('something went wrong')
+})
---
+++ models/groceriesModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const groceriesSchema = new Schema(
+    {
+        dairy: {type: String, required: true},
+        produce: {type: String, required: true},
+        snacks: { type: String, required: true}
+    }
+)
